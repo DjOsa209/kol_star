a9dabe196a04c3cb2c53a466fc73f6d9c16b2e65
ma rui <rui.ma1@transsion.com>
2026-09-14 14:12:15 +0800

    fix: localize table export action

--- vue-pure-admin/src/views/table/high/excel/index.vue
+++ vue-pure-admin/src/views/table/high/excel/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useColumns } from "./columns";
+import { fieldLabel } from "@/utils/fieldI18n";
 
 const { columns, dataList, exportExcel } = useColumns();
 </script>
@@ -7,7 +8,7 @@ const { columns, dataList, exportExcel } = useColumns();
 <template>
   <div>
     <el-button type="primary" class="mb-5!" @click="exportExcel">
-      导出
+      {{ fieldLabel("导出") }}
     </el-button>
     <pure-table row-key="id" border :data="dataList" :columns="columns" />
   </div>
